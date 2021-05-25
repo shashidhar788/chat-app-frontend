77ae3ee5059f1f54a7488addb87417caa4521ea9
shashidhar788 <rsreddy1378@gmail.com>
2021-05-25 18:36:52 -0400

    login auth connected with backend

--- src/components/AuthComponents/Login.jsx
+++ src/components/AuthComponents/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import AuthService from '../../services/authService';
 
 const Login = () => {
+
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    
+    const submitForm = (e) =>{
+        e.preventDefault();
+        AuthService.login({ email, password })
+                    .then(res=>{
+                        console.log(res);
+                    })
+                    .catch(err=>console.log(err));
+
+        /* axios.post('http://localhost:3001/login',
+        {email,password})
+        .then(res=>{
+            console.log("login post " , res);
+        })
+        .catch(err=> console.log(err)); */
+        console.log('login clicked' , `email = ${email}, password=${password}`);
+    }
+
     return(
         <div>
             <h2> Chat Login</h2>
+            <div>
+                <form onSubmit={submitForm}>
+                    <div>
+                        <input value={email} required='required'
+                        type="text"
+                        onChange={ e=> setEmail(e.target.value)} 
+                        placeholder="Login Email"></input>
+                    </div>
+                    
+                    <div>
+                        <input value={password} required='required'
+                        type="text"
+                        onChange={e=> setPassword(e.target.value) } 
+                        placeholder="Password"></input>
+                    </div>
+                    
+                    <button>Login</button>
+
+                    <div>
+                        <p> Don't have an account? Please <Link to="/register">Register</Link></p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
--- src/components/AuthComponents/Register.jsx
+++ src/components/AuthComponents/Register.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return(
         <div>
-            <h2> Registration Page</h2>
+        <h2> Registration Page</h2>
+        <div>
+            <form>
+                <div>
+                    <input placeholder="First Name"></input>
+                </div>
+
+                <div>
+                    <input placeholder="Last Name"></input>
+                </div>
+
+                <div>
+                    <input placeholder="Email"></input>
+                </div>
+                
+                <div>
+                    <input placeholder="Password"></input>
+                </div>
+
+                <div>
+                    <input placeholder="Re-enter Password"></input>
+                </div>
+
+                <button>Register</button>
+            </form>
+
+            <p> Already have an account? <Link to="/login"> Login!</Link> </p>
+
+
         </div>
+    </div>
     );
 }
 
