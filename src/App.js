8e1b1006c56ee9b07bb2bc348ef5cf9f40fa6d89
shashidhar788 <rsreddy1378@gmail.com>
2021-05-25 15:35:10 -0400

    added router, fixed switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import React from 'react';
 import './App.css';
+import Chat from './components/chatComponents/ChatComponent';
+import Login from './components/AuthComponents/Login';
+import Register from './components/AuthComponents/Register';
+
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <h2> Chat App</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <h2> Chat App</h2>
+        <Switch>
+          <Route exact path='/' component={Chat} />
+          <Route exact path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route render={()=><h1>404 page not found</h1>}/>
+        </Switch>
+
+      </div>
+    </Router>
+
   );
 }
 
